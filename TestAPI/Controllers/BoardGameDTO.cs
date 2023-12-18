b239714e8801b2bf951b1959cfd6a21579075c0f
NguyenLoiTan <loitannguyen111@gmail.com>
2023-12-18 09:05:44 +0700

    Update test

---
+++ TestAPI/Controllers/BoardGameDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+namespace TestAPI.DTO
+{
+	public class BoardGameDTO
+	{
+		[Required]
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public int? Year { get; set; }
+	}
+}
